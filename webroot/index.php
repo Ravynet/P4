8618e698e1b905f214d6fea65e825185a210958c
Yann <yann.ravinet@gmail.com>
2018-04-28 16:43:35 +0200

    Update

--- webroot/index.php
+++ webroot/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-require_once ('../app/config/constant.php');
-require_once (LIB.DS.'init.php');
-require_once (CONF.DS.'config.php');
+require_once('../app/config/constant.php');
+require_once(LIB . DS . 'init.php');
+require_once(CONF . DS . 'config.php');
 
 // RUN App
 App::run($uri);
