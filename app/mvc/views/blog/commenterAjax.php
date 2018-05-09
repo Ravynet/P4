ac99a2b8000cdce34855be7cee34de8502dccb1e
Yann <yann.ravinet@gmail.com>
2018-05-10 01:42:02 +0200

    Create view commenterAjax.php

---
+++ app/mvc/views/blog/commenterAjax.php
@@ -0,0 +1,15 @@
+<div class="comment-section-container">
+    <div class="comment-section-author">
+        <div class="comment-section-name">
+            <div class="auteur">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <p><?= htmlspecialchars($data->getComAuteur()) ?></p>
+            </div>
+            <p>Le <?= $data->getComDate() ?></p>
+        </div>
+    </div>
+    <div class="comment-section-text">
+        <p><?= $data->getComContenu()?></p>
+        <a class="hollow button" href="signaler?<?=$data->getComId()?>">Signaler ce commentaire</a>
+    </div>
+</div>
